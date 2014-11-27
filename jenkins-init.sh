6c1b8cb702e9eb95ce10a5968f5c753dd93ec888
Carlos Sanchez <carlos@apache.org>
2014-11-27 11:50:57 +0100

    Fix the su execution to work with params and keep env vars

--- jenkins-init.sh
+++ jenkins-init.sh
@@ -11,7 +11,12 @@ if [[ $# -lt 1 ]] || [[ "$1" == "--"* ]]; then
   find /usr/share/jenkins/plugins/ -type f -exec cp {} $JENKINS_HOME/plugins/ \;
 
   chown -R jenkins:jenkins $JENKINS_HOME
-  su - jenkins /usr/local/bin/jenkins.sh "$@"
+  if [[ -z "$@" ]]; then
+    exec su jenkins -c "/usr/local/bin/jenkins.sh"
+  else
+    PARAMS="$@"
+    exec su jenkins -c "/usr/local/bin/jenkins.sh \"$PARAMS\""
+  fi
 fi
 
 # As argument is not jenkins, assume user want to run his own process, for sample a `bash` shell to explore this image
